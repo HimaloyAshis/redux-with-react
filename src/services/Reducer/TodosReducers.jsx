29d921567767bebe3f0cd2fa31776a8e82dce252
himaloyash <himaloyashis98@gmail.com>
2023-08-19 11:50:25 +0600

    store created

--- src/services/Reducer/TodosReducers.jsx
+++ src/services/Reducer/TodosReducers.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import { GET_TODOS_FAILED, GET_TODOS_REQUEST, GET_TODOS_SUCCESS } from "../TodosConstant/TodosConstants";
 
-const TodosReducers = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const initialState = {
+    isLoading :false,
+    todos :[],
+    error: null
+}
 
-export default TodosReducers;
+const todosReducers = (state = initialState, action)=>{
+    switch (action.type) {
+        case GET_TODOS_REQUEST: 
+            return {
+                ...state, 
+                isLoading:true
+            };
+
+        case GET_TODOS_SUCCESS: 
+            return {
+                isLoading:false,
+                todos: action.payload,
+                error: null
+            };
+    
+        case GET_TODOS_FAILED: 
+            return {
+                isLoading:false,
+                todos: [],
+                error: action.payload
+            };
+    
+        default:
+            return state;
+    }
+}
+
+export default todosReducers;
